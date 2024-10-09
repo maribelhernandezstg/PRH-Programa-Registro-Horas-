eaa90f98115a3b4f4d853254d7466fdcd7591290
Isaac Espinoza <81329145+Wonderedcoot4@users.noreply.github.com>
2024-10-08 23:04:34 -0600

    Cambios

--- PRH/src/pages/Asesorados.tsx
+++ PRH/src/pages/Asesorados.tsx
@@ -1,13 +1,41 @@
 import { Container, Row, Col } from "react-bootstrap";
 import "../App.css";
 import { Filters } from "../components/Filters/Filters";
+import AsesoresTable from "../components/tables/TableAsesores"; 
+import { useState } from "react";
 
 const Asesorados = () => {
+  
+  const [searchAsesorado, setSearchAsesorado] = useState("");
+  const [searchMateria, setSearchMateria] = useState("");
+  const [searchAsesor, setSearchAsesor] = useState("");
+
+  
+  const registrosAsesorados = [
+    { asesorado: "Maria Lopez", matricula: "202321", carrera: "LDS", genero: "Femenino" },
+    { asesorado: "Carlos Perez", matricula: "202456", carrera: "ITC", genero: "Masculino" },
+  ];
+
+  
+  const filteredRegistros = registrosAsesorados.filter(registro =>
+    registro.asesorado.toLowerCase().includes(searchAsesorado.toLowerCase()) &&
+    registro.carrera.toLowerCase().includes(searchMateria.toLowerCase()) &&
+    registro.matricula.toLowerCase().includes(searchAsesor.toLowerCase())
+  );
+
   return (
     <Container className="window-container">
-      <Row className="">
+      <Row>
         <p className="title-container">Asesorados</p>
+        
         <Filters />
+        {/* Tabla de Asesorados */}
+        <Col>
+          <div className="table-container">
+            {/* Pasamos los registros filtrados a AsesoresTable */}
+            <AsesoresTable registros={filteredRegistros} />
+          </div>
+        </Col>
       </Row>
     </Container>
   );
--- PRH/src/pages/Excel.tsx
+++ PRH/src/pages/Excel.tsx
@@ -1,10 +1,31 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+
 const Excel = () => {
   return (
     <Container>
-      <Row className="mt-5">
-        <Col>
-          <h1>Bienvenido a la carga de Excel</h1>
+      <Row className="justify-content-center mt-5">
+        <Col md={6} className="text-center">
+          
+          <h2 className="mb-4">Cargar un Excel de Asesores</h2>
+          <p>(Ejemplo de columnas: Nombre, Matrícula, Carrera, Género)</p>
+
+          {/*Cargar Excel */}
+          <Form>
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label>Selecciona el archivo Excel:</Form.Label>
+              <Form.Control type="file" />
+            </Form.Group>
+            <Button variant="primary" className="mb-4">
+              Cargar Excel
+            </Button>
+          </Form>
+
+          {/*Generar Reporte*/}
+          <h4>Generar un reporte de Excel</h4>
+          <p>
+            Esto lo genera y dejara descargar en una carpeta a tu eleccion
+          </p>
+          <Button variant="success">Descargar Excel</Button>
         </Col>
       </Row>
     </Container>
